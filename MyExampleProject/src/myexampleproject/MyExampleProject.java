6732c4f01acb8342ba4f0c93cb4738caee35c583
Daniel <Daniel@192.168.1.144>
2017-01-25 18:06:58 -0700

    more stuff

--- MyExampleProject/src/myexampleproject/MyExampleProject.java
+++ MyExampleProject/src/myexampleproject/MyExampleProject.java
@@ -15,7 +15,7 @@ public class MyExampleProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //I made a change
     }
     
 }
